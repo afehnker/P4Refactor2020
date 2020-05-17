3c86c9bebec1e88a9c9ff7354b10a8df694865dc
Ansgar <fehnker@gmail.com>
2020-05-18 00:14:12 +0200

    Version 7.0 plus strategy pattern.

--- src/main/java/refactordemo/Movie.java
+++ src/main/java/refactordemo/Movie.java
@@ -1,57 +1,54 @@
 package refactordemo;
-public class Movie  {
-    public static final int  CHILDRENS = 2;
-    public static final int  REGULAR = 0;
-    public static final int  NEW_RELEASE = 1;
 
-    private String _title;
-    private int _priceCode;
+public class Movie {
+	public static final int CHILDRENS = 2;
+	public static final int REGULAR = 0;
+	public static final int NEW_RELEASE = 1;
 
-    public Movie(String name, int priceCode) {
-        _title = name;
-        _priceCode = priceCode;
-    }
+	private String _title;
+	private Price _price;
 
-    public int getPriceCode() {
-        return _priceCode;
-    }
+	public Movie(String name, int priceCode) {
+		_title = name;
+		setPriceCode(priceCode);
 
-    public void  setPriceCode(int newPrice){
-        _priceCode = newPrice;
-
-    }
-
-    public String getTitle(){
-        return _title;
-    }
-
-	double getCharge(int daysRented) {
-		double thisAmount = 0;
-		switch (getPriceCode()) {
-		    case Movie.REGULAR:
-		        thisAmount += 2;
-		        if (daysRented > 2)
-		            thisAmount += (daysRented - 2) * 1.5;
-		        break;
-		    case Movie.NEW_RELEASE:
-		        thisAmount += daysRented * 3;
-		        break;
-		    case Movie.CHILDRENS:
-		        thisAmount += 1.5;
-		        if (daysRented > 3)
-		            thisAmount += (daysRented - 3) * 1.5;
-		        break;
-		}
-		return thisAmount;
 	}
 
-	int getPoints(int daysRented) {
+	public int getPriceCode() {
+		return _price.getPriceCode();
+	}
+
+	public void setPriceCode(int newPrice) {
+		switch (newPrice) {
+		case REGULAR:
+			_price = new RegularPrice();
+			break;
+		case CHILDRENS:
+			_price = new ChildrensPrice();
+			break;
+		case NEW_RELEASE:
+			_price = new NewReleasePrice();
+			break;
+		default:
+			throw new IllegalArgumentException("Incorrect Price Code");
+		}
+	}
+
+	public String getTitle() {
+		return _title;
+	}
+
+	int getPoints(int dayRented) {
 		int points = 1;
-	
+
 		// add bonus for a two day new release rental
-		if ((_priceCode == Movie.NEW_RELEASE) && daysRented > 1)
+		if ((getPriceCode() == NEW_RELEASE) && dayRented > 1)
 			points++;
+		// show figures for this rental
 		return points;
 	}
 
+	double getCharge(int daysRented) {
+		return _price.getCharge(daysRented);
+	}
 }
