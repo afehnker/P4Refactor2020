454c7c8687b0070caed5d5c65e59b63e1a4127f9
Ansgar <fehnker@gmail.com>
2020-05-18 00:07:53 +0200

    Version 6.0

--- src/main/java/refactordemo/Movie.java
+++ src/main/java/refactordemo/Movie.java
@@ -25,4 +25,33 @@ public class Movie  {
         return _title;
     }
 
+	double getCharge(int daysRented) {
+		double thisAmount = 0;
+		switch (getPriceCode()) {
+		    case Movie.REGULAR:
+		        thisAmount += 2;
+		        if (daysRented > 2)
+		            thisAmount += (daysRented - 2) * 1.5;
+		        break;
+		    case Movie.NEW_RELEASE:
+		        thisAmount += daysRented * 3;
+		        break;
+		    case Movie.CHILDRENS:
+		        thisAmount += 1.5;
+		        if (daysRented > 3)
+		            thisAmount += (daysRented - 3) * 1.5;
+		        break;
+		}
+		return thisAmount;
+	}
+
+	int getPoints(int daysRented) {
+		int points = 1;
+	
+		// add bonus for a two day new release rental
+		if ((_priceCode == Movie.NEW_RELEASE) && daysRented > 1)
+			points++;
+		return points;
+	}
+
 }
