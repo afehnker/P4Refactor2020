40113a7b345993440dedc3098fd1ca5a1e2287e4
Ansgar <fehnker@gmail.com>
2020-05-18 09:36:11 +0200

    A bit of clean up before the refactoring hour.

--- src/main/java/refactordemo/Movie.java
+++ src/main/java/refactordemo/Movie.java
@@ -1,28 +1,29 @@
 package refactordemo;
-public class Movie  {
-    public static final int  CHILDRENS = 2;
-    public static final int  REGULAR = 0;
-    public static final int  NEW_RELEASE = 1;
 
-    private String _title;
-    private int _priceCode;
+public class Movie {
+	public static final int CHILDRENS = 2;
+	public static final int REGULAR = 0;
+	public static final int NEW_RELEASE = 1;
 
-    public Movie(String name, int priceCode) {
-        _title = name;
-        _priceCode = priceCode;
-    }
+	private String _title;
+	private int _priceCode;
 
-    public int getPriceCode() {
-        return _priceCode;
-    }
+	public Movie(String name, int priceCode) {
+		_title = name;
+		_priceCode = priceCode;
+	}
 
-    public void  setPriceCode(int newPrice){
-        _priceCode = newPrice;
+	public int getPriceCode() {
+		return _priceCode;
+	}
 
-    }
+	public void setPriceCode(int newPrice) {
+		_priceCode = newPrice;
 
-    public String getTitle(){
-        return _title;
-    }
+	}
+
+	public String getTitle() {
+		return _title;
+	}
 
 }
--- src/test/java/refactordemo/CustomerTest.java
+++ src/test/java/refactordemo/CustomerTest.java
@@ -1,4 +1,5 @@
 package refactordemo;
+
 import org.junit.Test;
 
 import refactordemo.Customer;
@@ -9,29 +10,28 @@ import static junit.framework.TestCase.assertEquals;
 
 public class CustomerTest {
 
-    @Test
-    public void testOwed() {
-        Customer Pete = new Customer("Peter");
-        Movie Shouf = new Movie("Shouf Shouf Habibi",1);
-        Movie Black = new Movie("Black Book",2);
+	@Test
+	public void testOwed() {
+		Customer Pete = new Customer("Peter");
+		Movie Shouf = new Movie("Shouf Shouf Habibi", 1);
+		Movie Black = new Movie("Black Book", 2);
 
-        Pete.addRental(new Rental(Shouf,2));
-        Pete.addRental(new Rental(Black,1));
+		Pete.addRental(new Rental(Shouf, 2));
+		Pete.addRental(new Rental(Black, 1));
 
-        assertEquals(7.5,Pete.getOwed());
-        
-    }
-    
-    @Test
-    public void testPoints() {
-        Customer Pete = new Customer("Peter");
-        Movie Shouf = new Movie("Shouf Shouf Habibi",1);
-        Movie Black = new Movie("Black Book",2);
+		assertEquals(7.5, Pete.getOwed());
 
-        Pete.addRental(new Rental(Shouf,2));
-        Pete.addRental(new Rental(Black,1));
+	}
 
-        
-        assertEquals(3,Pete.getPoints());
-    }
+	@Test
+	public void testPoints() {
+		Customer Pete = new Customer("Peter");
+		Movie Shouf = new Movie("Shouf Shouf Habibi", 1);
+		Movie Black = new Movie("Black Book", 2);
+
+		Pete.addRental(new Rental(Shouf, 2));
+		Pete.addRental(new Rental(Black, 1));
+
+		assertEquals(3, Pete.getPoints());
+	}
 }
