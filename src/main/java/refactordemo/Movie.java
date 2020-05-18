05e4b2bc2326c1d50e1e531d3118a6fb0a876de9
Ansgar <fehnker@gmail.com>
2020-05-18 11:29:24 +0200

    Version 6.0. Move get CHarge and getPoints to movie

--- src/main/java/refactordemo/Movie.java
+++ src/main/java/refactordemo/Movie.java
@@ -26,4 +26,32 @@ public class Movie {
 		return _title;
 	}
 
+	double getCharge(int daysRented) {
+		double thisAmount = 0;
+		switch (_priceCode) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (daysRented > 2)
+				thisAmount += (daysRented - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += daysRented * 3;
+			break;
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (daysRented > 3)
+				thisAmount += (daysRented - 3) * 1.5;
+			break;
+		}
+		return thisAmount;
+	}
+
+	int getPoints(int daysRented) {
+		// add bonus for a two day new release rental
+		int frequentRenterPoints = 1;
+		if ((_priceCode == Movie.NEW_RELEASE) && daysRented > 1)
+			frequentRenterPoints++;
+		return frequentRenterPoints;
+	}
+
 }
