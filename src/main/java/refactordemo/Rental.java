f798f805423f1779c25afc67174f7678661ad45a
Ansgar <fehnker@gmail.com>
2021-05-17 11:45:33 +0200

    End of Demo 3

--- src/main/java/refactordemo/Rental.java
+++ src/main/java/refactordemo/Rental.java
@@ -41,7 +41,7 @@ class Rental {
 		int frequentRenterPoints=1;
 	
 		// add bonus for a two day new release rental
-		if ((_movie.getPriceCode() == _movie.NEW_RELEASE) && _daysRented > 1)
+		if ((_movie.getPriceCode() == Movie.NEW_RELEASE) && _daysRented > 1)
 			frequentRenterPoints++;
 		// show figures for this rental
 		return frequentRenterPoints;
